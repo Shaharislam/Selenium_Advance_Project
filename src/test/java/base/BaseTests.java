39bbd3279b4605dc3c063223dfc1ab1dc29b302b
Rakibul Shahar <78203754+Shaharislam@users.noreply.github.com>
2023-05-11 20:26:29 +0600

    Apply POM

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,12 +1,18 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
+import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver");
@@ -14,6 +20,14 @@ public class BaseTests {
         driver.get("https://the-internet.herokuapp.com");
         driver.manage().window().setSize(new Dimension(375,812));
         System.out.println(driver.getTitle());
+
+        homePage = new HomePage(driver);
+
+        WebElement inputLink = driver.findElement(By.linkText("Inputs"));
+        inputLink.click();
+
+        List<WebElement> links = driver.findElements(By.tagName("span"));
+        System.out.println(links.size());
         driver.quit();
     }
 
